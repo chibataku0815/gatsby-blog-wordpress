33d57f2095f6ad1adcfae3a7a357f26fd1ce6a03
千葉工 <chibataku0815@gmail.com>
2019-02-18 22:05:52 +0900

    return style.module Navbar

--- src/components/Organisms/Navbar/Navbar.js
+++ src/components/Organisms/Navbar/Navbar.js
@@ -1,12 +1,9 @@
 // @flow
 
 import React from 'react'
-import classNames from 'classnames'
 import { Link, StaticQuery, graphql } from 'gatsby'
 import { Logo } from 'components/atoms/'
 import style from './style.module.scss'
-
-const moduleClass = classNames(style.navLink, 'nav-link')
 
 const Navbar = () => (
   <StaticQuery
@@ -29,7 +26,7 @@ const Navbar = () => (
             {data.wordpressWpApiMenusMenusItems.items.map(item => (
               <li className="nav-item" key={item.wordpress_id}>
                 <Link
-                  className={moduleClass}
+                  className={style.navLink}
                   to={item.url}
                   key={item.wordpress_id}
                 >
