7443df6b9b936501cdfa5d2f2b6603b0258e4d7d
千葉工 <chibataku0815@gmail.com>
2019-02-27 17:45:02 +0900

    [wip] theme styling

--- src/templates/page.js
+++ src/templates/page.js
@@ -7,9 +7,9 @@ import Layout from '../components/Layout'
 export const PageTemplate = ({ title, content }) => {
   return (
     <section>
-      <div className="container">
+      <div className="container my-5">
         <div className="row">
-          <div className="col-lg-10 mx-auto">
+          <div className="col-lg-8 card py-3">
             <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
               {title}
             </h2>
@@ -17,6 +17,9 @@ export const PageTemplate = ({ title, content }) => {
               className="content"
               dangerouslySetInnerHTML={{ __html: content }}
             />
+          </div>
+          <div className="col-lg-4">
+            <p className="card">サイドバー</p>
           </div>
         </div>
       </div>
